4194da82ea9bfbb6e67c87641b32a3744eb04a92
Nate Marti <nate_marti@sil.org>
2021-10-30 18:32:48 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+# https://docs.python.org/3/distutils/setupscript.html
+
+import glob
+from distutils.core import setup
+
+setup(
+    name='Snap Settings',
+    version=0.1,
+    description="Set basic snap settings in a user-friendly window.",
+    author="Nate Marti",
+    author_email="nate_marti@sil.org",
+    url="https://github.com/wasta-linux/snap-settings",
+    packages=['snapsettings'],
+    package_data={'snapsettings': ['README.md']},
+    scripts=['bin/basic-snap-settings'],
+    data_files=[
+        ('share/polkit-1/actions', glob.glob('data/actions/*')),
+        ('share/icons/hicolor/scalable/apps', glob.glob('data/icons/*.svg')),
+        ('share/traffic-cop/ui', glob.glob('data/ui/*.glade')),
+        ('share/applications', glob.glob('data/*.desktop')),
+    ]
+)
